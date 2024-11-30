a5818c40cec899ddbaea30c9e78e048626e36531
Matias Gimenez <p.matias.gimenez@hotmail.es>
2024-11-30 09:31:02 -0300

    cambios en index - grafico con export- correccion catch message

---
+++ assets/js/grafica.js
@@ -0,0 +1,20 @@
+function prepararGraficos(fechas,valores,divisa){
+    const chartDOM = document.getElementById("myChart");
+    if(myChart != null){
+        myChart.destroy()
+    }
+    myChart = new Chart(chartDOM, {
+            type: 'line',
+            data:{
+                labels: fechas,
+                datasets: [{
+                    label: divisa,
+                    data: valores,
+                    backgroundColor: '#FD0054',
+                }]
+            }
+
+    });
+}
+
+export {prepararGraficos}
